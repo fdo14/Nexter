c1a6468a585c29887d68d1fa29c8b763e0080ad0
Jack <jack@Jacks-MacBook-Air.local>
2019-04-04 19:03:52 -0400

    new changes

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+const port = process.env.PORT || 3000
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/views/index.html'));
+})
+
+app.listen(port);
